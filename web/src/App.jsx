669ed6856239f42fe38b6c6afa96eba7d118a0c4
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-12 20:42:57 +0500

    signup login page add

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import CreatePost from "./components/CreatePost";
 import Bar from "./components/Bar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div className=" p-2 min-h-screen bg-gradient-to-br from-[#ffb347] to-[#ffcc33]">
-      <Bar />
-      <CreatePost />
+      {/* <Bar />
+      <CreatePost /> */}
+      {/* <Signup /> */}
+      <Login />
     </div>
   );
 };
---
+++ web/src/pages/Signup.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Signup = () => {
+  const handleSubmit = () => {};
+  return (
+    <div className="bg-blue-200 min-h-screen flex justify-center items-center">
+      <div className=" bg-white px-8 pt-10 pb-6 w-[360px]">
+        <p className=" text-center font-semibold text-4xl mb-6 text-blue-600">
+          Register
+        </p>
+        <div>
+          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Username"
+              className="p-2 border-2 "
+            />
+            <input type="email" placeholder="email" className="p-2 border-2 " />
+            <input
+              type="password"
+              placeholder="password"
+              className="p-2 border-2 "
+            />
+            <label
+              htmlFor="file"
+              className="flex items-center gap-2 cursor-pointer"
+            >
+              {/* <img src={addAvatar} alt="addavatar" className="w-10" /> */}
+              <span className=" text-blue-800">Add Your Image</span>
+            </label>
+            <input type="file" name="" id="file" className="hidden" />
+            <button
+              type="submit"
+              className=" bg-blue-400 text-white p-2 text-lg hover:rounded-md transition-all mt-5"
+            >
+              Signup
+            </button>
+          </form>
+        </div>
+        <div className=" text-center my-4 text-blue-900">
+          {/* Already Have Account? <Link to="/login">Login</Link> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
